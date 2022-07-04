2ff9f7310f0ede2fb82bb803be4f281132ae5368
Buore <buore@Buores-MacBook-Pro.local>
2022-07-04 07:26:38 -0400

    update conftest use headless chrome

--- conftest.py
+++ conftest.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture(autouse=True, scope='class')
 def setup(request):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     request.cls.driver = driver
 
